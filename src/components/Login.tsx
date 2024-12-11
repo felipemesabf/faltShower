4e68916dae8fe4fa4fc8b4a96b58e129c17eedb7
Mesa, Luis <felipe.mesa@schoolmessenger.com>
2024-12-11 16:15:08 -0500

    Final App

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -17,24 +17,50 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-card">
-        <h1>Iniciar Sesión</h1>
-        <input
-          type="text"
-          placeholder="Nombre de usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleLogin}>Entrar</button>
+    <div className='general-container'>
+      <div className="login-container">
+        <div className="login-card">
+          <h1>Iniciar Sesión</h1>
+          <input
+            type="text"
+            placeholder="Nombre de usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Entrar</button>
+        </div>
       </div>
-    </div>
+      <div className="message-container">
+        <h1>Bienvenidos al ApartaShower de Pipe y Mil</h1>
+        <p>
+          Finalmente nos lanzamos a la independencia, y queremos que tú formes parte de este nuevo comienzo.
+          Te extendemos la invitación para que vengas y conozcas nuestro nuevo hogar… nuestro nuevo comienzo.
+          Tú has sido invitado porque has sido parte de nuestro proceso, y por eso queremos compartir nuestra
+          felicidad contigo.  
+        </p>
+        <p>
+          Te invitamos a visitarnos, a compartir con nosotros una comida y una charla agradable. Como sabes,
+          estamos empezando y a nuestro hogar aún le faltan cosas que iremos consiguiendo con el tiempo, pero
+          si tú deseas ayudarnos con algún detalle, será muy bien recibido.
+        </p>
+        <p>
+          Adjunto a tu invitación viene un usuario y una contraseña para que puedas entrar y echar un vistazo
+          a las cosas que nos hacen falta. Allí dentro, elige con cuidado, porque una vez que apartes algún
+          producto, no podrás deseleccionarlo. Además, los productos que un usuario ya haya seleccionado no 
+          podrán ser seleccionados por otros.
+        </p>
+        <p>
+          ¡Te esperamos con los brazos abiertos en nuestro nuevo hogar!
+        </p>
+      </div>
+  </div>
+
   );
 };
 
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -26,6 +26,12 @@ const ProductList = () => {
     <div>
     <button onClick={()=>logout()}>Logout</button>
     <h1>Listado de Productos</h1>
+    <div className="message-container">
+        <p>
+        "Si entre los productos que ves a continuación no encuentras algo que consideres imprescindible, ¡no te preocupes! Confiamos en tu criterio y sabemos que sabes lo que se necesita en un hogar. Adelante, estaremos encantados de recibir lo que desees brindarnos."
+        </p>
+      </div>
+    
     <div className="product-list-container">
       {products.map((product) => (
         <ProductItem key={product.id} product={product} />
