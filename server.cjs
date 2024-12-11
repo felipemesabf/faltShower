d2d59adc7833bc8ce9007d77bd9e4547b252b881
Mesa, Luis <felipe.mesa@schoolmessenger.com>
2024-12-11 18:12:15 -0500

    add final changes

--- server.cjs
+++ server.cjs
@@ -11,7 +11,24 @@ server.use((req, res, next) => {
   next();
 });
 
+const originalRender = router.render;
+router.render = (req, res) => {
+  if (req.path === '/users') {
+    const data = res.locals;
+    const newData = data.data.map((obj)=>{
+      delete obj.psw;
+      return obj
+    })      
+    const transformedData = [...newData];
+
+    return res.jsonp(transformedData);
+  }
+  return originalRender(req, res);
+};
+
+
 server.use(router);
+server.use(middlewares)
 server.listen(4000, () => {
   console.log('JSON Server is running',4000);
 });
